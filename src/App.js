4cf9c9fcd80b54202daa6f43ef02329ef6fd6ebf
alwaysalazycoder <gs314315316@gmail.com>
2023-02-03 19:40:19 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App= () => {
         <div>Geolocation is not enabled</div>
     ) : coords ? (
         <table>
+          
             <tbody>
                 <tr>
                     <td>latitude</td>
